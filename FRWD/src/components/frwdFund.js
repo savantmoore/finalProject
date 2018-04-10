8f7ca09827c40e41b7b0b1ed1a1b918ec0e4f201
Anthony Savant Moore <savant@frwdinc.com>
2018-04-10 11:36:36 -0400

    Update frwdFund.js

--- FRWD/src/components/frwdFund.js
+++ FRWD/src/components/frwdFund.js
@@ -21,6 +21,26 @@ const frwdFund = () =>  {
 	        <Button className="nav-btn" block>
 	    		<Link className="link" to="assumptions">Income</Link>
 	    	</Button>
+	    <!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://frwdapp.us18.list-manage.com/subscribe/post?u=e4fa99a4d45714237e499e9ed&amp;id=4423afd375" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">App Releases in 10 Days! Save $500 with FRWD</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e4fa99a4d45714237e499e9ed_4423afd375" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
         </div>
     );
 }
