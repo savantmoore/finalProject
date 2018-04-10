3b10b817574882acd20f5d62b6c98d16993443d8
Anthony Savant Moore <savant@frwdinc.com>
2018-04-10 12:12:03 -0400

    Update Landing.js

--- FRWD/src/components/Landing.js
+++ FRWD/src/components/Landing.js
@@ -14,7 +14,6 @@ const Landing = () =>  {
             	<Link className="link" to="frwd-fund">I understand - take me to the app</Link>
             </Button>
           </Jumbotron>
-        <footer><p class="copyright">Copyright 2018, FRWD Inc.</p></footer>
         </Grid>
     );
 }
