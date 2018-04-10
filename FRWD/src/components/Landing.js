23c8c67ff87cfc1c695b7a785af3a2eab74631ee
Anthony Savant Moore <savant@frwdinc.com>
2018-04-10 12:10:34 -0400

    Update Landing.js

--- FRWD/src/components/Landing.js
+++ FRWD/src/components/Landing.js
@@ -10,11 +10,11 @@ const Landing = () =>  {
           <Jumbotron id="landing-jumbotron">
             <h1>FRWD App</h1>
             <h6>Everyone should save 14%</h6>
-        <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us18.list-manage.com","uuid":"e4fa99a4d45714237e499e9ed","lid":"4423afd375"}) })</script>
             <Button className="nav-btn">
             	<Link className="link" to="frwd-fund">I understand - take me to the app</Link>
             </Button>
           </Jumbotron>
+        <footer><p class="copyright">Copyright 2018, FRWD Inc.</p></footer>
         </Grid>
     );
 }
